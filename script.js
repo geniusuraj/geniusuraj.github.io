f09b15a53973fde1c27d651986b65691b870810e
geniusuraj <geniusurajpy@gmail.com>
2023-11-09 15:51:57 +0530

    Final touchup

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const nav = document.getElementById('nav-menu');
-const menuToggle = document.querySelector('.menu-toggle');
+document.addEventListener('DOMContentLoaded', (event) => {
+    const nav = document.getElementById('nav-menu');
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelectorAll('.nav-link');
 
-menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
+    menuToggle.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+
+    function hideMenu() {
+        nav.classList.remove('active');
+    }
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', hideMenu);
+    });
 });
